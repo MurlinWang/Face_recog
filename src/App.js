42ab059ce216f0cbae4a8be47280923c06b3a068
Merlin Wang <1343810048@qq.com>
2022-04-18 21:36:49 -0400

    update api call

--- src/App.js
+++ src/App.js
@@ -133,7 +133,7 @@ function App() {
     // only make POST request on file selection
     if (event.target.value) {
       fetch(DROPDOWN_API_ENDPOINT, {
-        method: 'GET',
+        method: 'POST',
         body: JSON.stringify({ "fileName": event.target.value })
       }).then(response => response.json())
       .then(data => {
